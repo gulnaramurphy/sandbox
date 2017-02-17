417ae52d8955b1ddc427b97df35425d6f592e6fd
gulnaramurphy7 <murphy.gulnara@gmail.com>
2017-02-17 10:19:05 -0500

    added new test

---
+++ src/main/java/ru/stqa/pft/sandbox/TestGul.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by gmurphy on 2/17/2017.
+ */
+public class TestGul {
+
+  @Test
+  public void testEvent() {
+  }
+}
